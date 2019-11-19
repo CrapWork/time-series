946ec68c8704ad22685bd58f6e12b67dbd41b0b8
Fredrick Lambuth <flambuth@gmail.com>
2019-11-19 14:54:44 -0600

    this is frustrating to fix my awk script

--- acquire.py
+++ acquire.py
@@ -3,9 +3,6 @@ import pandas as pd
 import os
 
 def get_fitbit_data():
-    
-
-def get_store_data(use_cache=True):
     """
     Reads the merged csv in the PWD. Makes it a dataframe. This dataset was made from a BASH script that 
     calls 
@@ -14,3 +11,22 @@ def get_store_data(use_cache=True):
     return blob
 
 
+# os.system('ls -l')
+
+def get_data(use_cache=True):
+    if use_cache and os.path.exists('blob.csv'):
+        return pd.read_csv('blob.csv')
+    os.system('./make_big_csv.sh')
+    return pd.read_csv('blob.csv')
+
+
+def clean_data(df):
+    df = df.set_index('Date')
+    for i in df.select_dtypes(include='object').columns:
+        print(i)
+        df[i] = df[i].str.replace(',', '')
+        df[i] = df[i].astype('float64')
+        df.info()
+    # df.astype('float64',inplace=True).dtypes
+
+    return df
